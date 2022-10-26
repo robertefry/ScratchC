ecb513a13595912b09671b4ff22cb4c711669034
Robert E Fry <inbox@robertfry.xyz>
2022-10-27 00:28:53 +0100

    remove benchmark.hh in favour of hayai

--- scratch/include/Scratch.hh
+++ scratch/include/Scratch.hh
@@ -7,7 +7,6 @@
 #include "util/stringlist/format.hh"
 #include "util/stringlist/stl.hh"
 #include "util/logging.hh"
-#include "util/benchmark.hh"
 #include "util/lifetime.hh"
 #include "util/statistics.hh"
 
